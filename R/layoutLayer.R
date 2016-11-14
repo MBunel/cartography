929d1a15d275ff7be5e6abfccea717f644e514cf
Timothée Giraud <timothee.giraud@ums-riate.fr>
2016-11-14 11:59:15 +0100

    layout

--- R/layoutLayer.R
+++ R/layoutLayer.R
@@ -49,9 +49,19 @@ layoutLayer <- function(title = "Title of the map, year",
                         col = "black", coltitle = "white", theme = NULL, 
                         bg = NULL, scale = 0, frame = TRUE, north = FALSE, 
                         south = FALSE, extent = NULL){
-  
+  extent <- nuts0.sf
+  library(sf)
   if (!is.null(extent)){
-    sp::plot(extent, border = NA, col = NA, add = FALSE)
+    if (is(extent, "Spatial")){
+      sp::plot(extent, border = NA, col = NA, add = FALSE)
+    }
+  
+    if (is(extent, "sf")){
+      
+      st_geometry(extent) <- st_centroid(extent)
+    class(st_geometry(extent))
+        plot(extent,  add = FALSE)
+    }
     mapExtent <- par()$usr
   }else {
     mapExtent <- par()$usr
